Self-attention mask bugfix, remove end_span embedding

data_loader.py
@@ -82,20 +82,20 @@ class DataLoader(object):
         return bert_tokens, bert_label_action, bert_label_start, bert_label_end, bert_seq_width, token_start_indices
 
     def _split_multi_span(self, seq):
-        seq_out = [0]
+        sid = 0
+        seq_out = [sid]
         seq_width = [1]
         for si, i in enumerate(seq):
-            sid = si + 1
             if ',' in i:
-                slst = list(map(self.to_int, i.split(',')))[:self.max_sp_len] +\
-                        [self.max_len]
+                slst = list(map(self.to_int, i.split(',')))
+                slst = slst[:self.max_sp_len] + [sid] * int(len(slst) < self.max_sp_len)
                 seq_out.extend(slst)
                 seq_width.append(len(slst))
             else:
                 seq_out.append(self.to_int(i))
                 sw = 1
                 if seq_out[-1] > 0:
-                    seq_out.append(self.max_len)
+                    seq_out.append(sid)
                     sw += 1
                 seq_width.append(sw)
         return seq_out, seq_width
@@ -111,7 +111,7 @@ class DataLoader(object):
         end_seq = [k.split("|")[1].split("#")[1] for k in seq]
         action_seq = [self.tag2idx.get(tag) for tag in action_seq]
         tokens = ['[CLS]'] + tokens
-        action_seq = [1] + action_seq
+        action_seq = [self.tag2idx.get('DELETE')] + action_seq
         start_seq, seq_width = self._split_multi_span(start_seq)
         end_seq, _ = self._split_multi_span(end_seq)
         bert_tokens, bert_label_action, bert_label_start, bert_label_end, bert_seq_width, token_start_idxs = self._split_to_wordpieces_span(tokens, action_seq, start_seq, seq_width, end_seq, seq_width)
@@ -172,7 +172,6 @@ class DataLoader(object):
                     rem = sp_width[j][k2]
                     batch_tags[j][k2][:rem] = tags[j][k:k+rem]
                 rem -= 1
-        batch_tags = np.where(batch_tags > max_subwords_len - 1, max_subwords_len - 1, batch_tags)
         return batch_tags
 
     def data_iterator(self, data, shuffle=False):
@@ -217,7 +216,6 @@ class DataLoader(object):
 
             # batch length
             batch_len = len(sentences)
-            batch_max_sp_len += 1
 
             # compute length of longest sentence in batch
             batch_max_subwords_len = max([len(s[0]) for s in sentences])
@@ -240,9 +238,9 @@ class DataLoader(object):
                 batch_token_starts.append(token_starts)
 
             batch_action = self.copy_data(batch_len, max_subwords_len, action, self.tag_pad_idx)
-            batch_start = self.copy_data_3d(batch_len, max_subwords_len + 1, start, 0, sp_width, batch_max_sp_len)
-            batch_end = self.copy_data_3d(batch_len, max_subwords_len + 1, end, 0, sp_width, batch_max_sp_len)
-            batch_sp_width = self.copy_data(batch_len, max_subwords_len + 1, sp_width, 0)
+            batch_start = self.copy_data_3d(batch_len, max_subwords_len, start, 0, sp_width, batch_max_sp_len)
+            batch_end = self.copy_data_3d(batch_len, max_subwords_len, end, 0, sp_width, batch_max_sp_len)
+            batch_sp_width = self.copy_data(batch_len, max_subwords_len, sp_width, 0)
 
             # since all data are indices, we convert them to torch LongTensors
             batch_data = torch.tensor(batch_data, dtype=torch.long).to(self.device)

multi_headed_additive_attn.py
@@ -36,10 +36,10 @@ class Additive_Attention(nn.Module):
             weights: A tensor with shape [batch, length_q, length_kv]
         """
         def attention_bias(inputs, inf=-1e9):
-            mask = inputs
-            ret = (1.0 - mask) * inf
+            ret = (1.0 - inputs) * inf
             ret = ret.unsqueeze(dim=1)
-            ret = ret.unsqueeze(dim=1)
+            if len(ret.shape) < 4:
+                ret = ret.unsqueeze(dim=1)
             return ret
 
         bias = attention_bias(mask)
@@ -209,7 +209,7 @@ class MultiHeadedAttention(nn.Module):
 
         # 2) Calculate and scale scores.
         query = query / math.sqrt(dim_per_head)
-        
+
         # batch x num_heads x query_len x key_len
         #query_key = torch.matmul(query, key.transpose(2, 3))
 

sequence_tagger.py
@@ -19,12 +19,13 @@ from multi_headed_additive_attn import MultiHeadedAttention
 
 # span classifier based on self-attention
 class SpanClassifier(nn.Module):
-    def __init__(self, hidden_dim, max_relative_position):
+    def __init__(self, hidden_dim, max_relative_position, dropout=0.2):
         super(SpanClassifier, self).__init__()
         self.layer_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
         self.span_st_attn = MultiHeadedAttention(1, hidden_dim, max_relative_positions=max_relative_position)
         self.span_ed_attn = MultiHeadedAttention(1, hidden_dim, max_relative_positions=max_relative_position)
         self.comb_emb = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.dropout = dropout
         if max_relative_position > 0.0:
             print("Setting max_relative_position to {}".format(max_relative_position))
 
@@ -35,17 +36,19 @@ class SpanClassifier(nn.Module):
     def upd_hid(self, attn_w, hid):
         hidc = (hid.unsqueeze(1) * attn_w.unsqueeze(-1)).sum(2)
         hidc = self.comb_emb(torch.cat((hid, hidc), -1))
-        hidc = F.relu(hidc, inplace=True)
+        hidc = F.dropout(F.relu(hidc, inplace=True), p=self.dropout, training=self.training)
         return hidc
 
-    def forward(self, hid, sp_width, max_sp_len):
+    def forward(self, hid, sp_width, max_sp_len, attention_mask):
         sts, eds, masks = [], [], []
         hid1, hid2 = hid, hid
         for i in range(max_sp_len):
-            masks.append(torch.logical_and(sp_width, i <= sp_width).float())
-            sts.append(self.span_st_attn(hid1, hid1, hid1, mask=masks[-1], type="self")) # [batch, seq, seq]
+            mask = torch.logical_and(sp_width > 0, i < sp_width).float()
+            masks.append(mask)
+            mask = mask.unsqueeze(-1) * attention_mask.unsqueeze(1)
+            sts.append(self.span_st_attn(hid1, hid1, hid1, mask=mask, type="self")) # [batch, seq, seq]
             hid1 = self.upd_hid(sts[-1], hid1)
-            eds.append(self.span_ed_attn(hid2, hid2, hid2, mask=masks[-1], type="self")) # [batch, seq, seq]
+            eds.append(self.span_ed_attn(hid2, hid2, hid2, mask=mask, type="self")) # [batch, seq, seq]
             hid2 = self.upd_hid(eds[-1], hid2)
         return torch.stack(sts, -2), torch.stack(eds, -2), torch.stack(masks, -1)
 
@@ -68,14 +71,17 @@ def span_loss(start_dist, end_dist, start_positions, end_positions, seq_masks):
     span_ed_loss = token_classification_loss_v2(end_dist, end_positions, seq_masks)
     return span_st_loss + span_ed_loss
 
+
 def clip_and_normalize(word_probs, epsilon):
     word_probs = torch.clamp(word_probs, epsilon, 1.0 - epsilon)
     return word_probs / word_probs.sum(dim=-1, keepdim=True)
+
 
 def convert_tokens_to_string(tokens):
     """ Converts a sequence of tokens (string) in a single string. """
     out_string = " ".join(tokens).replace(" ##", "").strip()
     return out_string
+
 
 def decode_into_string(source, label_action, label_start, label_end, label_mask):
     assert len(source) == len(label_action)
@@ -107,7 +113,6 @@ class BertForSequenceTagging(BertPreTrainedModel):
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
         self.span_classifier = SpanClassifier(config.hidden_size, 0.0)
-        self.end_span = nn.Embedding(1, config.hidden_size)
         self._rl_ratio = 0.5
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         #self._tokenizer = BertTokenizer.from_pretrained("./dialogue_model/")
@@ -125,18 +130,14 @@ class BertForSequenceTagging(BertPreTrainedModel):
             self._rl_ratio = 0.0
         logits = self.classifier(sequence_output) #[bs, seq, 2]
 
-        # Add last sequence position for end-of-span prediction
-        sequence_output = torch.cat((sequence_output,
-            self.end_span.weight[None].expand(sequence_output.shape[0], -1, -1)), 1)
-
         max_sp_len = labels_start.shape[-1]
-        start_dist, end_dist, loss_mask = self.span_classifier(sequence_output, sp_width, max_sp_len)
+        start_dist, end_dist, loss_mask = self.span_classifier(sequence_output, sp_width, max_sp_len, attention_mask)
         start_outputs = start_dist.argmax(dim=-1) # [batch, seq]
         end_outputs = end_dist.argmax(dim=-1) # [batch, seq]
 
         outputs = (logits, start_outputs, end_outputs)
-        labels_start = F.one_hot(labels_start, num_classes=labels_start.shape[1]) #[bs, seq + 1, sp_len, seq + 1]
-        labels_end = F.one_hot(labels_end, num_classes=labels_start.shape[1]) #[bs, seq + 1, sp_len, seq + 1]
+        labels_start = F.one_hot(labels_start, labels_start.shape[1]) #[bs, seq, sp_len, seq]
+        labels_end = F.one_hot(labels_end, labels_end.shape[1]) #[bs, seq, sp_len, seq]
         loss_span = span_loss(start_dist, end_dist, labels_start, labels_end, loss_mask.float())
 
         loss_fct = CrossEntropyLoss()